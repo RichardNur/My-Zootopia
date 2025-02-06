b78b1bfa5790a02ee6e8b47235158efcf94a315f
deburic <richard_nuernberger@outlook.com>
2025-02-06 09:37:57 +0100

    Seperated Data Fetcher and Animal Info Generator.

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,28 +1,5 @@
 import requests
-
-def load_data(animal):
-    """
-    Loads data from API Ninjas for an input animal.
-
-    :params: Loads the data from API Ninjas (url).
-    :return: data as list of dictionaries.
-    """
-
-    API_KEY = "tM4bpRLCaOmb2Cv82/3NnA==5zKHBUimftwuIXex" # Key to access data on AI Ninjas
-    URL = "https://api.api-ninjas.com/v1/animals"
-    headers = {"X-Api-Key": API_KEY}
-
-    specification = "?name=" + animal
-    animal_url = URL + specification
-
-    try:
-        res = requests.get(animal_url, headers=headers)
-        return res.json()
-
-    except requests.exceptions.Timeout:
-        return {"error": "Request timed out. Try again later."}
-    except requests.exceptions.RequestException as e:
-        return {"error": f"API request failed: {e}"}
+import data_fetcher
 
 def read_animal_info(animal, data):
     """
@@ -109,7 +86,7 @@ def main():
     animal = input("Enter the animal you want to load the content for (in html): ").strip()
 
     # Load animal data
-    data = load_data(animal)
+    data = data_fetcher.load_data(animal)
     # Load HTML template
     html_template = read_html_template('animals_template.html')
 
---
+++ data_fetcher.py
@@ -0,0 +1,26 @@
+import requests
+
+def load_data(animal):
+    """
+    Loads data from API Ninjas for an input animal.
+
+    :params: Loads the data from API Ninjas (url).
+    :return: data as list of dictionaries.
+    """
+
+    API_KEY = "tM4bpRLCaOmb2Cv82/3NnA==5zKHBUimftwuIXex" # Key to access data on AI Ninjas
+    URL = "https://api.api-ninjas.com/v1/animals"
+    headers = {"X-Api-Key": API_KEY}
+
+    specification = "?name=" + animal
+    animal_url = URL + specification
+
+    try:
+        res = requests.get(animal_url, headers=headers)
+        return res.json()
+
+    except requests.exceptions.Timeout:
+        return {"error": "Request timed out. Try again later."}
+    except requests.exceptions.RequestException as e:
+        return {"error": f"API request failed: {e}"}
+
