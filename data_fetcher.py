e9e54d782d7b56c221a457d734346ab27b8661ca
deburic <richard_nuernberger@outlook.com>
2025-02-06 09:40:14 +0100

    new project architecture

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,5 +1,10 @@
 import requests
 
+API_KEY = "tM4bpRLCaOmb2Cv82/3NnA==5zKHBUimftwuIXex"  # Key to access data on AI Ninjas
+headers = {"X-Api-Key": API_KEY}
+URL = "https://api.api-ninjas.com/v1/animals"
+
+
 def load_data(animal):
     """
     Loads data from API Ninjas for an input animal.
@@ -8,9 +13,6 @@ def load_data(animal):
     :return: data as list of dictionaries.
     """
 
-    API_KEY = "tM4bpRLCaOmb2Cv82/3NnA==5zKHBUimftwuIXex" # Key to access data on AI Ninjas
-    URL = "https://api.api-ninjas.com/v1/animals"
-    headers = {"X-Api-Key": API_KEY}
 
     specification = "?name=" + animal
     animal_url = URL + specification
